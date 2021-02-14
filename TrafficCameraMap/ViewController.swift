cdd719cfbfcba9a801a3ff2d1dab7354eaba89a5
Rudra <rudratheact@gmail.com>
2021-02-15 00:51:19 +0530

    Initial Commit

---
+++ TrafficCameraMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficCameraMap
+//
+//  Created by Rudra on 15/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
