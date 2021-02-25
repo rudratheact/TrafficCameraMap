d67a57e2979d44312e53782397a4d7d42d2fc3cb
RUDRA S M <45320438+rudratheact@users.noreply.github.com>
2021-02-26 02:14:03 +0530

    MVVM

---
+++ TrafficCameraMap/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  TrafficCameraMap
+//
+//  Created by Rudra on 15/02/21.
+//
+
+import Foundation
+
+// Camera details structure
+struct TrafficCam {
+    let camera_id: String
+    let image: String
+    let lat: Double
+    let long: Double
+}
