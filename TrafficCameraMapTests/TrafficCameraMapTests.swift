478e23f5cbc96a68ef58fd1743185489af3eff4c
RUDRA S M <45320438+rudratheact@users.noreply.github.com>
2021-02-26 02:15:16 +0530

    Test cases (Unit+UI)
    XCTest and XCUITest

--- TrafficCameraMapTests/TrafficCameraMapTests.swift
+++ TrafficCameraMapTests/TrafficCameraMapTests.swift
@@ -6,28 +6,68 @@
 //
 
 import XCTest
+import MapKit
+
 @testable import TrafficCameraMap
 
 class TrafficCameraMapTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var viewModelToText: TrafficCameraMap.ViewModel!
+    
+    var viewControllerToTest: TrafficCameraMap.ViewController!
+    
+    private func setUpViewControllers() {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        self.viewControllerToTest = (storyboard.instantiateViewController(withIdentifier: "ViewController") as! TrafficCameraMap.ViewController) //
+        self.viewControllerToTest.loadView()
+        self.viewControllerToTest.viewDidLoad()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.setUpViewControllers()
+        self.viewModelToText = TrafficCameraMap.ViewModel()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    override func tearDown() {
+        viewControllerToTest = nil
+        viewModelToText = nil
+        super.tearDown()
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testViewControllerIsComposedOfMapView() {
+        XCTAssertNotNil(viewControllerToTest.mapView, "ViewController under test is not composed of a MKMapView")
     }
-
+    
+    func testControllerConformsToMKMapViewDelegate() {
+        
+        XCTAssert(viewControllerToTest.conforms(to: MKMapViewDelegate.self), "ViewController under test does not conform to MKMapViewDelegate protocol")
+    }
+    
+    func testAPICall(){
+        viewModelToText.callAPI(mapView: viewControllerToTest.mapView)
+    }
+    
+    // MARK:- Model testing
+    
+    var result: TrafficCam!
+    var annotation: CamPoint!
+    
+    private func dummyRecords(){
+        self.result = TrafficCam(camera_id: "9702", image: "https://images.data.gov.sg/api/traffic-images/2021/02/6f7fee1a-343a-49d0-8fe6-675d981f187b.jpg", lat: 1.39466333, long: 103.83474601)
+        self.annotation = CamPoint(title: result.camera_id, coordinate: CLLocationCoordinate2DMake(result.lat, result.long))
+    }
+    
+    func testRecords(){
+        dummyRecords()
+        XCTAssertEqual(result.lat, annotation.coordinate.latitude)
+        XCTAssertEqual(result.camera_id, annotation.title)
+    }
+    
 }
+
+/* for Testing
+ TrafficCam(camera_id: "9702", image: "https://images.data.gov.sg/api/traffic-images/2021/02/61a3d1a4-0b6c-4135-851f-8f981011fb29.jpg", lat: 1.39474081, long: 103.81797086)
+ */
--- TrafficCameraMapUITests/TrafficCameraMapUITests.swift
+++ TrafficCameraMapUITests/TrafficCameraMapUITests.swift
@@ -26,6 +26,19 @@ class TrafficCameraMapUITests: XCTestCase {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
+        
+        app.windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .map).element.tap()
+        app.otherElements["6708"].tap()
+        
+        let kotaTinggiMap = app/*@START_MENU_TOKEN@*/.maps.containing(.other, identifier:"Kota Tinggi").element/*[[".maps.containing(.other, identifier:\"Singapore Strait\").element",".maps.containing(.other, identifier:\"Singapore Changi Airport (SIN)\").element",".maps.containing(.other, identifier:\"Singapore\").element",".maps.containing(.other, identifier:\"Kampung Pasir Gudang\").element",".maps.containing(.other, identifier:\"Johor Bahru\").element",".maps.containing(.other, identifier:\"Pulai\").element",".maps.containing(.other, identifier:\"Tampoi\").element",".maps.containing(.other, identifier:\"Plentong\").element",".maps.containing(.other, identifier:\"Sungai Tiram\").element",".maps.containing(.other, identifier:\"Tebrau\").element",".maps.containing(.other, identifier:\"Kota Tinggi\").element"],[[[-1,10],[-1,9],[-1,8],[-1,7],[-1,6],[-1,5],[-1,4],[-1,3],[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        kotaTinggiMap.tap()
+        app.otherElements["4704"].tap()
+        kotaTinggiMap.tap()
+        kotaTinggiMap.tap()
+        
+        let tebrauMap = app/*@START_MENU_TOKEN@*/.maps.containing(.other, identifier:"Tebrau").element/*[[".maps.containing(.other, identifier:\"Singapore Strait\").element",".maps.containing(.other, identifier:\"Selat Pandan\").element",".maps.containing(.other, identifier:\"Singapore\").element",".maps.containing(.other, identifier:\"Selat Jurong\").element",".maps.containing(.other, identifier:\"Yio Chu Kang\").element",".maps.containing(.other, identifier:\"Tengah Air Base (TGA)\").element",".maps.containing(.other, identifier:\"Seletar Airport (XSP)\").element",".maps.containing(.other, identifier:\"Kampung Pasir Gudang\").element",".maps.containing(.other, identifier:\"Johor Bahru\").element",".maps.containing(.other, identifier:\"Johor Strait\").element",".maps.containing(.other, identifier:\"Tampoi\").element",".maps.containing(.other, identifier:\"Plentong\").element",".maps.containing(.other, identifier:\"Sungai Tiram\").element",".maps.containing(.other, identifier:\"Tebrau\").element"],[[[-1,13],[-1,12],[-1,11],[-1,10],[-1,9],[-1,8],[-1,7],[-1,6],[-1,5],[-1,4],[-1,3],[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        tebrauMap.tap()
+        tebrauMap.tap()
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
